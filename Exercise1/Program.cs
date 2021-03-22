7e46bf8d9b6bde6dd7e55a2e4767c019ccce835b
wnsghks825 <junhwan825@gmail.com>
2021-03-23 01:31:40 +0900

    Maze_BFS

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -24,56 +24,6 @@ namespace Exercise
             new List<int>() { 3, 5 },
             new List<int>() { 4 }
         };
-
-        //DFS
-/*
-        bool[] visited = new bool[6];
-        //1) 우선 now 부터 방문하고, 
-        //2) now와 연결된 정점들을 하나씩 확인해서 [ 아직 미발견(미방문)] 상태라면 방문한다. 
-        //행렬 방식
-        public void DFS(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true;           //우선 now부터 방문하고
-
-            //배열 방식
-            for (int next = 0; next < 6; next++)
-            {
-                if (adj[now, next] == 0)    //연결되어 있지 않으면 스킵
-                    continue;
-                if (visited[next])         //이미 방문했다면 스킵
-                    continue;
-
-                //나와 연결된 정점들 찾기
-                DFS(next);
-            }
-        }
-        public void DFS2(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true;           //우선 now부터 방문하고
-
-            foreach(int next in adj2[now])  //연결되어 있지 않으면 스킵
-            {
-                if (visited[next])         //이미 방문했다면 스킵
-                    continue;
-
-                //나와 연결된 정점들 찾기
-                DFS2(next);
-            }
-        }
-
-        public void SearchAll()
-        {
-            visited = new bool[6];
-            for(int now = 0; now < 6; now++)
-            {
-                if (visited[now] == false)
-                    DFS(now);
-            }
-        }
-*/
-
         public void BFS(int start)
         {
             bool[] found = new bool[6];
