c23d5feb29782431d92dd04ba45a46428e72aa21
wnsghks825 <junhwan825@gmail.com>
2021-03-24 00:53:43 +0900

    Tree

---
+++ Exercise/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+        //노드가 있고, 데이터를 물고 있고, 자신과 연결된 자식들을 가지고 있다. 
+
+    }
+
+    class Program
+    {
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "전투" });
+                    node.Children.Add(new TreeNode<string>() { Data = "경제" });
+                    node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+                    root.Children.Add(node);
+                }
+
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Children.Add(node);
+                }
+
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "배경" });
+                    node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+                    root.Children.Add(node);
+                }
+            }
+            return root;
+        }
+
+        //순화?
+        static void PrintTree(TreeNode<string> root)
+        {
+            //접근
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> child in root.Children)
+                PrintTree(child);
+        }
+
+        //Tree를 건네주면 높이를 구해
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+            foreach (TreeNode<string> child in root.Children)
+            {
+                int newHeight = GetHeight(child) + 1;
+                if (height < newHeight)
+                    height = newHeight;
+                height = Math.Max(height, newHeight);
+            }
+
+            return height;
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode<string> root = new TreeNode<string>();
+
+            PrintTree(root);
+        }
+    }
+}
