94013093bfbd9157ab207d556a0972503b3a4df4
wnsghks825 <junhwan825@gmail.com>
2021-03-24 00:02:55 +0900

    Dijikstra

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,12 +7,12 @@ namespace Exercise
     {
         int[,] adj = new int[6, 6]
         {
-            {0,1,0,1,0,0 },
-            {1,0,1,1,0,0 },
-            {0,1,0,0,0,0 },
-            {1,1,0,0,1,0 },
-            {0,0,0,1,0,1 },
-            {0,0,0,0,1,0 }
+            { -1, 15, -1, 35, -1, -1 },
+            { 15, -1, 05, 10, -1, -1 },
+            { -1, 05, -1, -1, -1, -1 },
+            { 35, 10, -1, -1, 05, -1 },
+            { -1, -1, -1, 05, -1, 05 },
+            { -1, -1, -1, -1, 05, -1 }
         };
 
         List<int>[] adj2 = new List<int>[]
@@ -24,6 +24,69 @@ namespace Exercise
             new List<int>() { 3, 5 },
             new List<int>() { 4 }
         };
+
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+            Array.Fill(distance, Int32.MaxValue);
+
+            distance[start] = 0;
+            parent[start] = start;
+
+            while (true)
+            {
+                //제일 좋은 후보를 찾기(가까이에 있는)
+
+                //가장 유력한 후보의 거리와 번호를 저장한다.
+                int closest = Int32.MaxValue;
+                int now = -1;
+                for(int i = 0; i < 6; i++)
+                {
+                    //이미 방문한 정점은 스킵
+                    if (visited[i])
+                        continue;
+                    //아직 발견(예약)된 적이 없거나, 기존 후보보다 멀리 있다면 스킵
+                    if (distance[i] == Int32.MaxValue || distance[i] > closest)
+                        continue;
+                    //여태껏 발견한 좋은 후보라는 의미, 정보 갱신
+                    closest = distance[i];
+                    now = i;
+
+                }
+
+                //다음 후보가 하나도 없다.
+                if (now == -1)
+                    break;
+
+                //제일 좋은 후보를 찾았으니 방문한다. 
+                visited[now] = true;
+
+                //방문한 정점과 인접한 정점들을 조사해서, 상황에 따라 발견한 최단거리를 갱신한다. 
+                for(int next = 0; next < 6; next++)
+                {
+                    //연결되지 않은 정점 스킵
+                    if (adj[now, next] == -1)
+                        continue;
+                    //이미 방문한 정점 스킵
+                    if (visited[next])
+                        continue;
+
+                    //새로 조사된 정점의 최단거리를 계산한다. 
+                    int nextDist = distance[now] + adj[now, next];
+
+                    //만약 기존에 발견한 최단거리가 새로 조사된 최단거리보다 크면, 정보 갱신
+                    if (nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[start] = now;
+                    }
+                    
+                    distance[3] = 35;
+                }
+            }
+        }
         public void BFS(int start)
         {
             bool[] found = new bool[6];
